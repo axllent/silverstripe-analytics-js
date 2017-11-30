487d02f7bd4ee323d35f6279d5746a03b9bc6b02
Ralph Slooten <axllent@gmail.com>
2017-12-01 11:49:09 +1300

    Bugfix - prevent IE from opening email links twice

--- code/AnalyticsJS.php
+++ code/AnalyticsJS.php
@@ -203,7 +203,7 @@ class AnalyticsJS extends Extension
     {
         $repl = array(
             '!/\*[^*]*\*+([^/][^*]*\*+)*/!' => '', // Comments
-            '/(    |\n)/' => '', // soft tabs / new lines
+            '/(    |\n|\t)/' => '', // soft tabs / new lines
             '/\s?=\s?/' => '=',
             '/\s?==\s?/' => '==',
             '/\s?!=\s?/' => '!=',
